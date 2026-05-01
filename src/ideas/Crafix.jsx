f24507264ec8645d1eb3edb6c88846106290adc8
Dharamveer <102931119+dharamveerkz@users.noreply.github.com>
2026-05-01 12:29:00 +0530

    Create Crafix.jsx

---
+++ src/ideas/Crafix.jsx
@@ -0,0 +1,15 @@
+import ProjectCard from '../components/ProjectCard';
+
+export default function Crafix() {
+  return (
+    <ProjectCard
+      id="005"
+      name="Crafix"
+      highlight="Cart"
+      description="Turn your ideas into unique, personalized wearable designs with effortless customization and creative freedom."
+      tags={['Customisation', 'Creativity', 'Style']}
+      starred={true}
+      href="/prototypes/crafix"
+    />
+  );
+}
