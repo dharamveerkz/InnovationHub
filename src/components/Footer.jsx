68dd75025b29876aa9c14438319156e97d611225
Dharamveer <102931119+dharamveerkr@users.noreply.github.com>
2026-05-18 14:29:09 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <footer>
         <div className="footer-name">Innovation Hub.</div>
         <div className="social-links">
-          <a className="social-link" href="https://github.com/dharamveerkz" target="_blank" rel="noopener">GitHub</a>
+          <a className="social-link" href="https://github.com/dharamveerkr" target="_blank" rel="noopener">GitHub</a>
           <a className="social-link" href="https://linkedin.com/in/dharamveerkr" target="_blank" rel="noopener">LinkedIn</a>
           <a className="social-link" href="https://www.instagram.com/imdharamvrr" target="_blank" rel="noopener">Instagram</a>
           <a className="social-link" href="mailto:dharamveerkumar774@mail.com">Email</a>
